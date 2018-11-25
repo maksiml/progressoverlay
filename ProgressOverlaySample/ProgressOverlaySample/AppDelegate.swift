52668597ddcba21798135ddffcb605a51ee953a5
Maksim Libenson <maksiml@ceemax.com>
2018-11-25 09:27:20 -0800

    Add sample project

---
+++ ProgressOverlaySample/ProgressOverlaySample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ProgressOverlaySample
+//
+//  Created by Maksim Libenson on 11/25/18.
+//  Copyright © 2018 Maksim Libenson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ProgressOverlaySample/ProgressOverlaySample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ProgressOverlaySample
+//
+//  Created by Maksim Libenson on 11/25/18.
+//  Copyright © 2018 Maksim Libenson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
